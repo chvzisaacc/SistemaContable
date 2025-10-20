674101fd679e8fc9f075b140e73c7637854279d3
Isaac Chavez <isae_chavezc15@unicah.edu>
2025-10-19 20:01:37 -0600

    NUEVO CAMBIO

--- Capa de Presentación/Form1.Designer.cs
+++ Capa de Presentación/Form1.Designer.cs
@@ -45,9 +45,9 @@
             // 
             button2.Location = new Point(182, 266);
             button2.Name = "button2";
-            button2.Size = new Size(273, 98);
+            button2.Size = new Size(447, 98);
             button2.TabIndex = 1;
-            button2.Text = "HOLAAA";
+            button2.Text = "helooo";
             button2.UseVisualStyleBackColor = true;
             // 
             // Form1
