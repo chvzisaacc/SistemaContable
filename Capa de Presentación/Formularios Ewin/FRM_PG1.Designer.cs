82813fb641fcdff6a13133b1301a91547a613b11
Edwin Rene Acosta Rodriguez <Edwinrodriguez10moa@gmail.com>
2025-10-21 22:59:10 -0600

    revisen

---
+++ Capa de Presentación/Formularios Ewin/FRM_PG1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Capa_de_Presentación.Formularios_Ewin
+{
+    partial class FRM_PG1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.GradientInactiveCaption;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(223, 234);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(450, 34);
+            textBox1.TabIndex = 0;
+            textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.GradientInactiveCaption;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(59, 234);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 33);
+            label1.TabIndex = 1;
+            label1.Text = "Usuario:";
+            label1.Visible = false;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.GradientInactiveCaption;
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(59, 288);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 33);
+            label2.TabIndex = 3;
+            label2.Text = "Contraseña:";
+            label2.Visible = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.GradientInactiveCaption;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(223, 285);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(450, 34);
+            textBox2.TabIndex = 2;
+            textBox2.Visible = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GradientInactiveCaption;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(271, 400);
+            button1.Name = "button1";
+            button1.Size = new Size(265, 71);
+            button1.TabIndex = 4;
+            button1.Text = "Iniciar Sesión";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // FRM_PG1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Imagen_de_WhatsApp_2025_10_21_a_las_22_22_16_14a2ddba;
+            ClientSize = new Size(1279, 719);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "FRM_PG1";
+            Text = "FRM_PG1";
+            Load += FRM_PG1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ Capa de Presentación/Formularios Ewin/FRM_PG1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_de_Presentación.Formularios_Ewin
+{
+    public partial class FRM_PG1 : Form
+    {
+        public FRM_PG1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FRM_PG1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
