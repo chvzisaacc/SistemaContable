3bcf592c986fb036b002c4bbb4bf61026ab7593c
Isaac Chavez <isae_chavezc15@unicah.edu>
2025-10-22 17:05:06 -0600

    Clase conexión agregada, si sale error asegurarse de instalar el nugget de Microsoft.Data.SqlCliente, y cambiar su server en la cadena para que funcione.

---
+++ Capa de acceso de datos/Clsconexion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace PRUEBA_LOGIN
+{
+    public class Clsconexion
+    {
+        string conexion = "Data Source=DESKTOP-NRLBBAA\\SQLEXPRESS;Initial Catalog=BD_Arquidiocesis;Integrated Security=True;TrustServerCertificate=True;";
+
+
+        public SqlConnection sc = new();
+
+        public Clsconexion()
+        {
+            sc.ConnectionString = conexion;
+        }
+        public void Abrir()
+        {
+            try
+            {
+                if (sc.State == ConnectionState.Closed)
+                    sc.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al abrir la conexión: " + ex.Message, ex);
+            }
+
+
+        }
+        public void Cerrar()
+        {
+            try
+            {
+                if (sc.State == ConnectionState.Open)
+                {
+                    sc.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al cerrar la conexión: " + ex.Message, ex);
+            }
+        }
+        
+    }
+}
