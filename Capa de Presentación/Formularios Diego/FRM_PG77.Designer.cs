5093c339c795d56d03f8bc53de18bf51ee7572e8
Diego Muñoz <diegomanuelm.sc@gmail,com>
2025-10-22 00:30:24 -0600

    Pantalla 77 Lista

---
+++ Capa de Presentación/Formularios Diego/FRM_PG77.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Capa_de_Presentación.Formularios_Diego
+{
+    partial class FRM_PG77
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_PG77));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel2 = new Panel();
+            panel5 = new Panel();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(panel5);
+            panel1.Location = new Point(35, 26);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(823, 476);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(20, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(97, 88);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(134, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Sacerdote";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(20, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Ingresos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(212, 139);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Gastos";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(413, 139);
+            label4.Name = "label4";
+            label4.Size = new Size(124, 25);
+            label4.TabIndex = 4;
+            label4.Text = "Caja Chica";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(610, 139);
+            label5.Name = "label5";
+            label5.Size = new Size(85, 25);
+            label5.TabIndex = 5;
+            label5.Text = "Bancos";
+            // 
+            // panel3
+            // 
+            panel3.Enabled = false;
+            panel3.Location = new Point(35, 193);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(823, 3);
+            panel3.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(175, 192);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(0, 0);
+            panel4.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(comboBox1);
+            panel2.Controls.Add(label6);
+            panel2.Location = new Point(194, 209);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(406, 232);
+            panel2.TabIndex = 8;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(43, 56, 143);
+            panel5.Location = new Point(191, 206);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(412, 238);
+            panel5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(77, 10);
+            label6.Name = "label6";
+            label6.Size = new Size(275, 32);
+            label6.TabIndex = 10;
+            label6.Text = "Seleccione una cuenta";
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.FromArgb(251, 203, 51);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(140, 61);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(43, 105);
+            label7.Name = "label7";
+            label7.Size = new Size(88, 25);
+            label7.TabIndex = 10;
+            label7.Text = "Monto:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(251, 203, 51);
+            textBox1.Location = new Point(137, 103);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(163, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(43, 56, 143);
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(105, 166);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 29);
+            button1.TabIndex = 13;
+            button1.Text = "Guardar y cerrar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // FRM_PG77
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(43, 56, 143);
+            ClientSize = new Size(907, 548);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Name = "FRM_PG77";
+            Text = "FRM_PG77";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Label label4;
+        private Panel panel3;
+        private Panel panel2;
+        private TextBox textBox1;
+        private Label label7;
+        private ComboBox comboBox1;
+        private Label label6;
+        private Panel panel4;
+        private Panel panel5;
+        private Button button1;
+    }
+}
